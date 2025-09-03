49d70b1eef1858a6981ea62423135aab4f16af67
Kitsanaz-Dev <kitsanaz.ctbd@gmail.com>
2025-09-03 10:22:06 +0700

    change directory of database.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = require('./app');
-const connectDB = require('./connectDB');
+const connectDB = require('./config/database');
 
 // Connect to the database
 connectDB();
