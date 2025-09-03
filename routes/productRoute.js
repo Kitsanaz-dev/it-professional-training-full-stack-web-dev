70a9823da4e209901afb20aaf2ecf74e6f5d833f
Kitsanaz-Dev <kitsanaz.ctbd@gmail.com>
2025-09-03 11:44:20 +0700

    update route and controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,5 +4,10 @@ const productController = require('../controllers/productController');
 
 router.get('/all-products', productController.getAllProducts);
 router.post('/create-product', productController.createProduct);
+router.get('/:id', productController.getProductById);
+router.put('/update-product/:id', productController.updateProduct);
+router.patch('/update-stock/:id', productController.updateStock);
+router.delete('/delete-product/:id', productController.deleteProduct);
+router.get('/low-stock', productController.getLowStockProducts);
 
 module.exports = router;
